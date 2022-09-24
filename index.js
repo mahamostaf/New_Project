1de04829f210174bf46e2a0b87dc265b70805cb3
mahamostaf <mahamoftah19@gmail.com>
2022-09-24 13:35:50 +0200

    task1

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-console.log("Hello");
+// var url = require("url");
+
+// var address = "https://githup.com.com/mahamostaf";
+// var endPoint=url.parse(address,true);
+// console.log(endPoint.host);
+
+const { write } = require("fs");
+const http = require("http");
+let port = 5000 ;
+const server = http.createServer(function(req,res){
+    let url = (req.url).split('/');
+    res.write(url[1]);
+    res.end();
+})
+server.listen(port);
