21f91846495d93d4720783b79b221e31d053d662
mahamostaf <mahamoftah19@gmail.com>
2022-09-24 13:57:33 +0200

    task1 Update

--- index.js
+++ index.js
@@ -6,10 +6,9 @@
 
 const { write } = require("fs");
 const http = require("http");
-let port = 5000 ;
-const server = http.createServer(function(req,res){
+let port = 8000 ;
+http.createServer(function(req,res){
     let url = (req.url).split('/');
     res.write(url[1]);
     res.end();
-})
-server.listen(port);
+}).listen(port);
